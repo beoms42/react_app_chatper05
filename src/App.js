c1806445b6586304bfcf089fc47a8cc5280fe009
yicho <yicho@spectra.co.kr>
2023-07-29 22:05:20 +0900

    07.29 P262 Chapter 05 end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Viewer from "./componemt/Viewer";
+import Controller from "./componemt/Controller";
+import { useState } from "react";
+
+/**
+ * P262 END
+ * @returns {JSX.Element}
+ * @constructor
+ */
 
 function App() {
+    const [count, setCount] = useState(0);
+    const handleSetCount = (value) => {
+        setCount(count + value);
+    };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Simple Counter</h1>
+        <section>
+            <Viewer count={count} />
+        </section>
+
+        <section>
+            <Controller handleSetCount={handleSetCount}/>
+        </section>
     </div>
   );
 }
