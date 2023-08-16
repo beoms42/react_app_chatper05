9b6cc1da7bfed958bb2c7da257a6a83df50e7821
yicho <yicho@spectra.co.kr>
2023-08-16 17:19:42 +0900

    08.03 P280 마운트 / 언마운트 언마운트 개념이 이해가 잘 안됨

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import './App.css';
 import Viewer from "./componemt/Viewer";
 import Controller from "./componemt/Controller";
-import { useState } from "react";
+import Even from "./componemt/Even";
+import { useState, useEffect } from "react";
 
 /**
- * P262 END
+ * P280 chapter 06
  * @returns {JSX.Element}
  * @constructor
  */
 
 function App() {
     const [count, setCount] = useState(0);
+    const [text, setText] = useState("");
     const handleSetCount = (value) => {
         setCount(count + value);
     };
+
+    const handleChangeText = (e) => {
+        setText(e.target.value);
+    }
+
+
+
+    // useEffect(() => {
+    //     console.log("업데이트 : ", count, text);
+    // }, [count, text]);
 
   return (
     <div className="App">
         <h1>Simple Counter</h1>
         <section>
-            <Viewer count={count} />
+            <input value={text} onChange={handleChangeText}/>
         </section>
 
         <section>
-            <Controller handleSetCount={handleSetCount}/>
+            <Viewer count={count} />
+            {count % 2 === 0 && <Even />}
+        </section>
+
+        <section>
+            <Controller handleSetCount={handleSetCount} />
         </section>
     </div>
   );
